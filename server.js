1badadb7b3122ca8a88d7f7d2abffa4c015616ab
Michael Pace <michael.g.pace@gmail.com>
2023-07-01 13:39:19 -0500

    server built to run

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict'
 
 const express = require('express');
-const {config} = require('dotenv');
+const {config} = require('dotenv').config();
 
-const PORT = process.env
+const PORT = process.env.PORT;
+
+const server = express();
+
+server.listen(PORT, () => console.log('Listening on port:', PORT));
